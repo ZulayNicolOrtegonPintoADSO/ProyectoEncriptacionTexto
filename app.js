d0363a20e2ebb339722d3fdad055d5726ca8b70c
ZulayNicolOrtegonPintoADSO <zulyortegon15@gmail.com>
2023-05-29 07:13:49 -0500

    feat:�  Código del html con un mínimo en vue
    Aquí está el  primer diseño del programa, no tiene nada de javascript

---
+++ app.js
@@ -0,0 +1,4 @@
+import { createApp } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.prod.js'
+import Mycomponent from './component/my-component.js'
+
+createApp(Mycomponent).mount('#app')
---
+++ component/my-component.js
@@ -0,0 +1,8 @@
+export default{
+    data(){
+        return{
+            centinela: false ,
+            desencriptar: false
+        }
+    }
+}
